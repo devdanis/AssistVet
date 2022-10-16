c04fbda51ee47c7bf639f38e0f649a533b9890a0
devdanis <104639930+devdanis@users.noreply.github.com>
2022-10-16 12:25:00 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -9,3 +9,40 @@ function menu() {
       x.style.position = "absolute";
     }
   }
+
+
+//   function prod() {
+//     var x = document.getElementById("nav__prod");
+//     console.log(x);
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//     }
+//     else {
+//       x.style.display = "block";
+//       x.style.position = "absolute";
+//     }
+//   }
+
+const btnProductos = document.getElementById('productos');
+const menuProductos = document.getElementById('nav__prod');
+const btnServicios = document.getElementById('servicios');
+const tocarBody = document.getElementsByTagName ("body")[0];
+
+
+btnProductos.addEventListener ('mouseover', () => {
+    menuProductos.style.display = "block";
+    menuProductos.style.position = "absolute";
+});
+
+menuProductos.addEventListener ('mouseleave', () => {
+    menuProductos.style.display = "none";
+});
+
+btnServicios.addEventListener ('mouseover', () => {
+    menuProductos.style.display = "none";
+    // menuProductos.style.position = "absolute";
+});
+
+tocarBody.addEventListener ("click", () => {
+    menuProductos.style.display = "none";
+});
